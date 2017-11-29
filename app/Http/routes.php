57dc70982edb2b5d266dd6791847ca06aaa604f0
Dave Hill <davehill140279@hotmail.com>
2017-11-29 12:43:51 +0000

    Admin area and css completed

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('admin/users', 'AdminUsersController');
+
+Route::get('/admin', function() {
+  return view('admin.index');
+});
